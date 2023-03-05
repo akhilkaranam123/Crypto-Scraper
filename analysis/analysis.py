c3f5a80801490c09355965c04083699184620df7
akhilkaranam123 <venkobarao.knl@gmail.com>
2023-03-05 13:57:58 +0530

    Update analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,14 +1,38 @@
+from matplotlib import pyplot as plt
 import seaborn as sns
 
-sns.boxplot(df_ethereum['Close'])
+#Find the outliers for Bitcoin Prices
+
 sns.boxplot(df_bitcoin['Close'])
-from matplotlib import pyplot as plt
 
-sns.distplot(df_bitcoin['Close'],kde = False)
-plt.show()
-sns.distplot(df_ethereum['Close'],kde = False)
+#Find the ouliers for Ethereum Prices
+
+sns.boxplot(df_ethereum['Close'])
+
+
+#Analysis of Patter for Bitcoin
+
+date_time = df_bitcoin['Date']
+data = df_bitcoin['Close']
+
+DF = pd.DataFrame()
+DF['value'] = data
+DF = DF.set_index(date_time)
+plt.plot(DF)
+plt.gcf().autofmt_xdate()
 plt.show()
 
+#Analysis of Patter for Ethereum
+
+date_time = df_ethereum['Date']
+data = df_ethereum['Close']
+
+DF = pd.DataFrame()
+DF['value'] = data
+DF = DF.set_index(date_time)
+plt.plot(DF)
+plt.gcf().autofmt_xdate()
+plt.show()
 
 
 
