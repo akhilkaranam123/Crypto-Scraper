81e43ef2a54dd0c3c9a7f7721e322122969d02d1
akhilkaranam123 <venkobarao.knl@gmail.com>
2023-03-05 14:02:18 +0530

    Adding Comments

--- main/main.py
+++ main/main.py
@@ -1,4 +1,6 @@
 import s3_connection
 import scraper
 
+# This file is to run the scripts with all the commands we want. If we can schedule this file to run we can get s3 files updated. 
+
 crypto_scrape('bitcoin','bitcoin_date.csv','C:\User\Desktop\')
